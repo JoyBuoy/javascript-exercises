79187617b6ed8382e5acdb17f852e0435481daec
JoyBuoy <136883858+JoyBuoy@users.noreply.github.com>
2024-05-01 21:49:54 -0700

    partial completion fof removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
+   const count = arguments[0].length;
+   let finalArray = [];
+   const argumentLength = arguments.length;
+   argumentLength = argumentLength-1;
 
+   for(let i = 0;i<count;i++){
+    for(let n=1; n<=argumentLength;n++){
+        if(arguments[0][i]!=arguments[n]){
+            finalArray.push(arguments[0][i]);
+        }
+    }
+   }
+   return finalArray;
 };
 
 // Do not edit below this line
