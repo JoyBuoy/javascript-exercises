51ebd6dc6f918947ee21aead52a5ea2f87d85968
JoyBuoy <136883858+JoyBuoy@users.noreply.github.com>
2024-05-01 19:18:32 -0700

    reversestring complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const givenArray = string.split("");
+    // get the length of the array
+    // run the loop those many times with the index from backwards.
+    // then convert this new array into a string.
+    noOfChar = givenArray.length;
+    let revArray = [];
+    let n=0;
+    for (let i=0;i<noOfChar;i++){
+        revArray[i] = givenArray[noOfChar-n-1];
+        n++;
+    }
+
+    revString = revArray.join("");
+    return revString;
 
 };
 
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString; 
