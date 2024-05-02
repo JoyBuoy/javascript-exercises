e79eaf624c15336db445cb111796d5309d9587cc
JoyBuoy <136883858+JoyBuoy@users.noreply.github.com>
2024-05-01 18:57:08 -0700

    repeatstrings complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    if (num <0){
+        return "ERROR";
+    }
+    else {
+        let finalstring = "";
+        for (let i=1;i<=num;i++){
+        finalstring = finalstring.concat(string);
+        }
+        return finalstring;
+    }
 
 };
 
